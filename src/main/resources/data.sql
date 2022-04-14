9d32df7d24079f4680eca15eb871e4b6821b6b12
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-14 20:24:34 -0300

    populate data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,13 @@
 USE alkemy;
 
 INSERT INTO roles (name, description)
-VALUES ('admin', 'Full application access')
+VALUES ('admin', 'Full application access');
+
+INSERT INTO roles (name, description)
+VALUES ('user', 'User level access');
+
+INSERT INTO roles (name, description)
+VALUES ('maintainer', 'Can read and update resources');
+
+INSERT INTO roles (name, description)
+VALUES ('developer', 'Developer access level');

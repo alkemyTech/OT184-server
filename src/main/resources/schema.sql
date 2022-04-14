4ba573596e6c423b51c652cdf6ee617603b46997
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-14 18:13:32 -0300

    fix sql schema, add default values

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE roles
 (
     id          INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(30) NOT NULL,
-    description VARCHAR(30) NULL,
-    created_at  DATETIME    NOT NULL,
-    updated_at  DATETIME    NOT NULL,
-    is_deleted  BOOL        NOT NULL
+    description VARCHAR(100) NULL,
+    created_at  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    is_deleted  BOOL        NOT NULL DEFAULT FALSE
 );

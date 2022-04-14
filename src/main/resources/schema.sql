51ee9ead5a8004608bf0f091af00d44d8934f2a4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-14 17:48:45 -0300

    add role schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
 USE alkemy;
+
+# Roles
+CREATE TABLE roles
+(
+    id          INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(30) NOT NULL,
+    description VARCHAR(30) NULL,
+    created_at  DATETIME    NOT NULL,
+    updated_at  DATETIME    NOT NULL,
+    is_deleted  BOOL        NOT NULL
+);

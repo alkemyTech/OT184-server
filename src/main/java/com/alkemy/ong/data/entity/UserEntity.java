53bafc15f6f8201a61b81625ace9ae111fc87bcf
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 12:11:22 -0300

    add non nullable to annotations

--- src/main/java/com/alkemy/ong/data/entity/UserEntity.java
+++ src/main/java/com/alkemy/ong/data/entity/UserEntity.java
@@ -10,16 +10,19 @@ public class UserEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "first_name")
+  @Column(name = "first_name", nullable = false)
   private String firstName;
 
-  @Column(name = "last_name")
+  @Column(name = "last_name", nullable = false)
   private String lastName;
 
+  @Column(unique = true, nullable = false)
   private String email;
 
+  @Column(nullable = false)
   private String password;
 
+  @Column(nullable = false)
   private String photo;
 
   @ManyToOne

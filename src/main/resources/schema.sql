fcf7873ed2d438f17f121b8b6af04b6891b4127f
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-15 15:26:54 -0300

    PR's corrections

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE news (
    content TEXT NOT NULL,
    image VARCHAR(255) NOT NULL,
    category_id BIGINT NOT NULL,
-   is_deleted BIT(1) NULL,
-   created_at timestamp NULL,
+   is_deleted BIT(1) NOT NULL,
+   created_at timestamp NOT NULL,
    updated_at timestamp NULL,
    CONSTRAINT pk_news PRIMARY KEY (id)
 );

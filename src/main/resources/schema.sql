5bfe0426ef0760f671cd6d6a353dea7951826f99
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 12:35:52 -0300

    use 255 length varchar

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,8 @@ USE alkemy;
 CREATE TABLE roles
 (
     id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(30)  NOT NULL,
-    description VARCHAR(100) NULL,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NULL,
     created_at  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     is_deleted  BOOL         NOT NULL DEFAULT FALSE
@@ -15,11 +15,11 @@ CREATE TABLE users
     id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     role_id    INT,
     FOREIGN KEY (role_id) REFERENCES roles (id),
-    first_name VARCHAR(20)  NOT NULL,
-    last_name  VARCHAR(20)  NOT NULL,
-    email      VARCHAR(30)  NOT NULL UNIQUE,
-    password   VARCHAR(50)  NOT NULL,
-    photo      VARCHAR(100) NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    photo      VARCHAR(255) NULL,
     created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     is_deleted BOOL         NOT NULL DEFAULT FALSE

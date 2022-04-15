1ae5dfdd9257ac3e2227c61518e19fd8a7dd1c9b
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 12:53:19 -0300

    populate users table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,15 @@ VALUES ('maintainer', 'Can read and update resources');
 
 INSERT INTO roles (name, description)
 VALUES ('developer', 'Developer access level');
+
+INSERT INTO users (role_id, first_name, last_name, email, password, photo)
+VALUES (1, 'Juan', 'Gomez', 'juangomez@mail.com', '12345678', 'https://www.somebucket.com/photos/image.jpg');
+
+INSERT INTO users (role_id, first_name, last_name, email, password, photo)
+VALUES (2, 'Maria', 'Perez', 'mariaperez@mail.com', 'qwerty', 'https://www.somebucket.com/photos/image.jpg');
+
+INSERT INTO users (role_id, first_name, last_name, email, password)
+VALUES (3, 'Jose', 'Gonzalez', 'josegonzalez@mail.com', 'zxcvb');
+
+INSERT INTO users (role_id, first_name, last_name, email, password)
+VALUES (1, 'Debora', 'Zalazar', 'deborazalar@mail.com', 'asdfgh');

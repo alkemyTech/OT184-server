d16d38d2d02fea08060e8b9ecd88d416865f9b50
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 12:06:04 -0300

    add user entity

---
+++ src/main/java/com/alkemy/ong/data/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.data.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  private String email;
+
+  private String password;
+
+  @ManyToOne
+  private RoleEntity role;
+}

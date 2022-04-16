474e05838c776b70c711a65aa289ceedd665ed71
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 23:27:57 -0300

    add activities schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
 USE alkemy;
+
+CREATE TABLE activities
+(
+    id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    content    TEXT         NOT NULL,
+    image      VARCHAR(255) NOT NULL,
+    created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    is_deleted BOOL         NOT NULL DEFAULT FALSE
+);

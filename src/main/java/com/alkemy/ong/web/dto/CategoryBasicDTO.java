b606b73e90bba0b9e5f47aa17f0bbe2f7481c17b
Cristian <cristianfcba@gmail.com>
2022-04-16 14:12:25 -0300

    CategoryBasicDTO created

---
+++ src/main/java/com/alkemy/ong/web/dto/CategoryBasicDTO.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CategoryBasicDTO {
+
+    private String name;
+}

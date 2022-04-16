345a3e7a5e5a53759eb5352fcfbe51d6b58d422c
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-16 17:46:18 -0300

    Added: entity, repository

--- src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
+++ src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
@@ -1,12 +1,17 @@
 package com.alkemy.ong.data.gateway;
 
+import com.alkemy.ong.data.repository.NewsRepository;
 import com.alkemy.ong.domain.gateway.NewsGateway;
 import com.alkemy.ong.web.controller.dto.NewsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DefaultGateway implements NewsGateway {
+
+    @Autowired
+    NewsRepository newsRepository;
     @Override
     public NewsDTO save(NewsDTO newsDTO) {
-
+        newsRepository.save(newsDTO);
         return new NewsDTO();
     }
 }

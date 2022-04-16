baa31983563a77a4eb71211963ce882e350cd629
Cristian <cristianfcba@gmail.com>
2022-04-16 14:37:22 -0300

    CategoryService created

--- src/main/java/com/alkemy/ong/domain/category/CategoryService.java
+++ src/main/java/com/alkemy/ong/domain/category/CategoryService.java
@@ -1,7 +1,15 @@
 package com.alkemy.ong.domain.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryGateway categoryGateway;
+
+    public Category findAll(){
+        return categoryGateway.findAll();
+    }
 }

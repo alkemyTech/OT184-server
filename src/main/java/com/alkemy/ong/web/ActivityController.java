cfd11475a961f413a0e967deb98711159b043a54
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-16 18:08:25 -0300

    add activity controller

---
+++ src/main/java/com/alkemy/ong/web/ActivityController.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/activities")
+public class ActivityController {
+
+  @PostMapping
+  public ResponseEntity<ActivityDto> save(@RequestBody ActivityDto activity) {
+    ActivityDto saveActivity = activityService.save(activity);
+    return ResponseEntity.status(HttpStatus.CREATED).body(saveActivity)
+  }
+}

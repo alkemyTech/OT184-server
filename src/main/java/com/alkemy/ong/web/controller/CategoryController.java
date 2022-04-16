4a9365ce1662b404f0c9b5f4cf94d806f88697a8
Cristian <cristianfcba@gmail.com>
2022-04-16 16:22:11 -0300

    add get category by id in controller

--- src/main/java/com/alkemy/ong/web/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/web/controller/CategoryController.java
@@ -1,11 +1,14 @@
 package com.alkemy.ong.web.controller;
 
+import com.alkemy.ong.domain.category.Category;
 import com.alkemy.ong.domain.category.CategoryService;
 import com.alkemy.ong.web.dto.CategoryBasicDTO;
+import com.alkemy.ong.web.dto.CategoryDTO;
 import com.alkemy.ong.web.mapper.CategoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +25,12 @@ public class CategoryController {
 
         CategoryBasicDTO categoryBasicDTOS = CategoryMapper.toDTOBasic(categoryService.findAll());
         return ResponseEntity.ok().body(categoryBasicDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryByID(@PathVariable Long id){
+        Category category = categoryService.findById(id);
+        CategoryDTO categoryDTO = CategoryMapper.toDTO(category);
+        return ResponseEntity.ok(categoryDTO);
     }
 }

7fd60aa1efa991114e066985d063c4e2e3c746c6
Cristian <cristianfcba@gmail.com>
2022-04-16 14:35:17 -0300

    Category model created

---
+++ src/main/java/com/alkemy/ong/domain/category/Category.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.domain.category;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Category {
+
+    private String name;
+
+    private String description;
+
+    private String image;
+}

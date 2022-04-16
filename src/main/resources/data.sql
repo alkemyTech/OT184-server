317db2bfd5909711d807589157004a75a293c7b4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-15 23:28:24 -0300

    add activities seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,31 @@
+USE alkemy;
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity one', 'do activity one description', 'https://www.somebucket.com/activity_one.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity two', 'do activity two description', 'https://www.somebucket.com/activity_two.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity three', 'do activity three description', 'https://www.somebucket.com/activity_three.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity four', 'do activity four description', 'https://www.somebucket.com/activity_four.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity five', 'do activity five description', 'https://www.somebucket.com/activity_five.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity six', 'do activity six description', 'https://www.somebucket.com/activity_six.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity seven', 'do activity seven description', 'https://www.somebucket.com/activity_seven.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity eight', 'do activity eight description', 'https://www.somebucket.com/activity_eight.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity nine', 'do activity nine description', 'https://www.somebucket.com/activity_nine.jpg');
+
+INSERT INTO activities (name, content, image)
+    VALUE ('activity ten', 'do activity ten description', 'https://www.somebucket.com/activity_ten.jpg');

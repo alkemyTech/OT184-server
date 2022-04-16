165b3a51eea8c77d793364d728a9a6267ac2352b
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-04-15 23:53:31 -0300

    add table creation script in schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,14 @@
 USE alkemy;
+CREATE TABLE members (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   facebook_url VARCHAR(255) NULL,
+   instagram_url VARCHAR(255) NULL,
+   linkedin_url VARCHAR(255) NULL,
+   image VARCHAR(255) NOT NULL,
+   `description` VARCHAR(255) NULL,
+   is_deleted BIT(1) NULL,
+   created_at datetime NULL,
+   update_at datetime NULL,
+   CONSTRAINT pk_members PRIMARY KEY (id)
+);

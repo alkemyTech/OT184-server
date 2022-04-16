ff333a28870ebcb01cbc0016f84390beccf71657
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-16 19:40:55 -0300

    rollback activity sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,1 @@
 USE alkemy;
-
-CREATE TABLE activities
-(
-    id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    content    TEXT         NOT NULL,
-    image      VARCHAR(255) NOT NULL,
-    created_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    is_deleted BOOL         NOT NULL DEFAULT FALSE
-);

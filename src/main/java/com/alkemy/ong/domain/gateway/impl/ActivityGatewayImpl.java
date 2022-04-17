47b66bc5062aeda3a1bc4d3f9e7f90888e8500f3
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-17 18:31:39 -0300

    add activity gateway implementation

---
+++ src/main/java/com/alkemy/ong/domain/gateway/impl/ActivityGatewayImpl.java
@@ -0,0 +1,32 @@
+package com.alkemy.ong.domain.gateway.impl;
+
+import com.alkemy.ong.data.entity.ActivityEntity;
+import com.alkemy.ong.data.repository.ActivityRepository;
+import com.alkemy.ong.domain.gateway.ActivityGateway;
+import com.alkemy.ong.domain.model.Activity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityGatewayImpl implements ActivityGateway {
+  @Autowired
+  ActivityRepository activityRepository;
+
+  @Override
+  public Activity save(Activity activity) {
+
+    ActivityEntity savedActivityEntity = activityRepository.save(
+        ActivityEntity.builder()
+            .name(activity.getName())
+            .content(activity.getContent())
+            .image(activity.getImage())
+            .build()
+    );
+
+    return Activity.builder()
+        .name(savedActivityEntity.getName())
+        .content(savedActivityEntity.getContent())
+        .image(savedActivityEntity.getImage())
+        .build();
+  }
+}

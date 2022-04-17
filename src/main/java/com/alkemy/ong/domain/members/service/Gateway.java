dd57a55d549215ca65ea9025482ae8976113bdf1
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-04-17 05:17:49 -0300

    Name Members to MembersModel, add interface Gateway

---
+++ src/main/java/com/alkemy/ong/domain/members/service/Gateway.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.domain.members.service;
+
+import com.alkemy.ong.domain.members.model.Members;
+
+import java.util.List;
+
+public interface Gateway {
+
+    List<Members> getAll();
+
+}

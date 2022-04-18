cab46f2013b7c5239ea305fd74d506c84d736f89
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-04-18 01:04:22 -0300

    Controller member creation

--- src/main/java/com/alkemy/ong/web/members/controller/MembersController.java
+++ src/main/java/com/alkemy/ong/web/members/controller/MembersController.java
@@ -1,15 +1,15 @@
 package com.alkemy.ong.web.members.controller;
 
-import com.alkemy.ong.data.members.service.DefaultGateway;
+
 import com.alkemy.ong.domain.members.model.Members;
 import com.alkemy.ong.domain.members.service.Gateway;
 import com.alkemy.ong.web.members.dto.MemberDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +24,25 @@ public class MembersController {
     public MembersController (Gateway gateway){
         this.gateway = gateway;
     }
+
+    @PostMapping
+    public ResponseEntity<MemberDTO> save(@Valid @RequestBody MemberDTO memberDTO){
+         Members members =
+                 gateway.save(
+                     Members
+                         .builder().name(memberDTO.getName())
+                         .facebookUrl(memberDTO.getFacebookUrl()).instagramUrl(memberDTO.getInstagramUrl())
+                         .linkedinUrl(memberDTO.getLinkedinUrl()).image(memberDTO.getImage())
+                         .description(memberDTO.getDescription()).build());
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(
+            MemberDTO
+                    .builder().id(members.getId()).name(members.getName())
+                    .facebookUrl(members.getFacebookUrl()).instagramUrl(members.getInstagramUrl())
+                    .linkedinUrl(members.getLinkedinUrl()).image(members.getImage())
+                    .description(members.getDescription()).build());
+    }
+
 
     @GetMapping
     public ResponseEntity<List<MemberDTO>> getAll(){

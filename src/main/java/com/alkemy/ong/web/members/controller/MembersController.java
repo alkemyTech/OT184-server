0a0e7c1aa07ac7658633e4ce0e72cda32f6494ad
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-04-18 13:04:10 -0300

    adding inner class MemberDTO in memberController

--- src/main/java/com/alkemy/ong/web/members/controller/MembersController.java
+++ src/main/java/com/alkemy/ong/web/members/controller/MembersController.java
@@ -3,7 +3,9 @@ package com.alkemy.ong.web.members.controller;
 import com.alkemy.ong.data.members.service.DefaultGateway;
 import com.alkemy.ong.domain.members.model.Members;
 import com.alkemy.ong.domain.members.service.Gateway;
-import com.alkemy.ong.web.members.dto.MemberDTO;
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("members")
 public class MembersController {
@@ -21,24 +24,41 @@ public class MembersController {
     private final Gateway gateway;
 
     @Autowired
-    public MembersController (Gateway gateway){
+    public MembersController(Gateway gateway) {
         this.gateway = gateway;
     }
 
     @GetMapping
-    public ResponseEntity<List<MemberDTO>> getAll(){
+    public ResponseEntity<List<MemberDTO>> getAll() {
         List<Members> membersModels = gateway.getAll();
         List<MemberDTO> memberDTO = new ArrayList<>();
+
         membersModels.forEach((Members) -> memberDTO.add(
                 MemberDTO
                         .builder().id(Members.getId()).name(Members.getName())
                         .facebookUrl(Members.getFacebookUrl()).instagramUrl(Members.getInstagramUrl())
                         .linkedinUrl(Members.getLinkedinUrl()).image(Members.getImage())
                         .description(Members.getDescription()).build()
-                ));
+        ));
         return ResponseEntity.ok().body(memberDTO);
     }
 
+    @Data
+    @Builder
+    private class MemberDTO {
+        private Long id;
+        @NotNull
+        private String name;
+        private String facebookUrl;
+        private String instagramUrl;
+        private String linkedinUrl;
+        @NotNull
+        private String image;
+        private String description;
 
-
+    }
 }
+
+
+
+

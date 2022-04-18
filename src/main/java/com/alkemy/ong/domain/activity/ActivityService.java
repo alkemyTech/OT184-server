b03e86d8385e4c80aa51a5bd85b353d9c21bfc68
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-18 15:00:02 -0300

    refactor: autowire constructor

--- src/main/java/com/alkemy/ong/domain/activity/ActivityService.java
+++ src/main/java/com/alkemy/ong/domain/activity/ActivityService.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ActivityService {
 
+  private final ActivityGateway activityGateway;
+
   @Autowired
-  ActivityGateway activityGateway;
+  public ActivityService(ActivityGateway activityGateway) {
+    this.activityGateway = activityGateway;
+  }
 
   public Activity save(Activity activity) {
     return activityGateway.save(activity);

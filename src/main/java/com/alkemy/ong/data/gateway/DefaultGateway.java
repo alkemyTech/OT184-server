3dda9236a0d3e3280312c5639c9690cfe7271aaf
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 05:54:45 -0300

    News Default Gateway created

--- src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
+++ src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
@@ -1,30 +1,11 @@
 package com.alkemy.ong.data.gateway;
 
-import com.alkemy.ong.data.entity.NewsEntity;
-import com.alkemy.ong.data.mapper.NewsModelMapper;
-import com.alkemy.ong.data.repository.NewsRepository;
 import com.alkemy.ong.domain.gateway.NewsGateway;
-import com.alkemy.ong.domain.model.News;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultGateway implements NewsGateway {
 
-    @Autowired
-    NewsRepository newsRepository;
-    @Autowired
-    NewsModelMapper newsModelMapper;
 
-    /**
-     * Recibe un objeto de tipo model, lo guarda en la db y devuelve el mismo objeto con la id generada.
-     * @param news
-     * @return news
-     */
-    @Override
-    public News save(News news) {
-        NewsEntity newsEntity = newsModelMapper.modelToEntity(news);
-        News returnModel = newsModelMapper.entityToModel(newsRepository.save(newsEntity));
-        return returnModel;
-    }
+
 }

b3ebd6bf2baa44ae1e774beeb6afa8f4d0e6c13f
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 06:05:43 -0300

    DefaultGateway methods created

--- src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
+++ src/main/java/com/alkemy/ong/data/gateway/DefaultGateway.java
@@ -1,11 +1,20 @@
 package com.alkemy.ong.data.gateway;
 
 import com.alkemy.ong.domain.gateway.NewsGateway;
+import com.alkemy.ong.domain.model.News;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultGateway implements NewsGateway {
 
 
+    @Override
+    public void delete(Long id) {
+        
+    }
 
+    @Override
+    public News findById(Long id) {
+        return null;
+    }
 }

0be49750e7b527b6066178b683a006bf0aaf1513
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 06:04:12 -0300

    NewsService interface's methods implemented

--- src/main/java/com/alkemy/ong/domain/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/service/impl/NewsServiceImpl.java
@@ -1,4 +1,6 @@
 package com.alkemy.ong.domain.service.impl;
+import com.alkemy.ong.domain.gateway.NewsGateway;
+import com.alkemy.ong.domain.model.News;
 import com.alkemy.ong.domain.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,4 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class NewsServiceImpl implements NewsService {
 
+    @Autowired
+    NewsGateway newsGateway;
+
+
+    @Override
+    public void delete(Long id) {
+        
+    }
+
+    @Override
+    public News findById(Long id) {
+        return null;
+    }
 }

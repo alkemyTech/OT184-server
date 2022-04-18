e7c9cdb397bee714740f5d41cc18de76d3bc6a04
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 07:03:22 -0300

    NewsController getDetails method implemented

--- src/main/java/com/alkemy/ong/web/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/web/controller/NewsController.java
@@ -1,7 +1,12 @@
 package com.alkemy.ong.web.controller;
 import com.alkemy.ong.domain.service.NewsService;
+import com.alkemy.ong.web.controller.dto.NewsDTO;
 import com.alkemy.ong.web.controller.mapper.NewsDTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,9 @@ public class NewsController {
     @Autowired
     NewsDTOMapper newsDTOMapper;
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<NewsDTO> getDetails(@PathVariable Long id){
+        NewsDTO returnDTO = newsDTOMapper.modelToDTO(newsService.getDetails(id));
+        return ResponseEntity.status(HttpStatus.FOUND).body(returnDTO);
+    }
 }

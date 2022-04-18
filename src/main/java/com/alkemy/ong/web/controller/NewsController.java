46781ad9618679941bf958f8a8032f12eb7b0d26
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 05:57:48 -0300

    NewsController method delete created

--- src/main/java/com/alkemy/ong/web/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/web/controller/NewsController.java
@@ -3,8 +3,11 @@ package com.alkemy.ong.web.controller;
 import com.alkemy.ong.domain.service.NewsService;
 import com.alkemy.ong.web.controller.mapper.NewsDTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -16,5 +19,9 @@ public class NewsController {
     @Autowired
     NewsDTOMapper newsDTOMapper;
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        newsService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }

52734fcfc2b6a931d1de7252c1b54c47fcdd2b14
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-18 02:01:55 -0300

    add activity model

--- src/main/java/com/alkemy/ong/domain/categories/Activity.java
+++ src/main/java/com/alkemy/ong/domain/categories/Activity.java
@@ -1,9 +1,14 @@
 package com.alkemy.ong.domain.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Activity {
   String name;
-
   String content;
-
   String image;
 }

5fb5ebd56d9fb5faf4b1e7d3bba5a68eebc0659c
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-18 14:52:54 -0300

    refactor: move user to package

---
+++ src/main/java/com/alkemy/ong/domain/user/User.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.domain.user;
+
+import com.alkemy.ong.domain.model.Role;
+
+public class User {
+  String firstName;
+
+  String lastName;
+
+  String email;
+
+  String password;
+
+  String photo;
+
+  Role role;
+}

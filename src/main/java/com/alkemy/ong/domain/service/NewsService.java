e83b42d22c83a6a2887920ebfb72810da12e378b
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 05:53:08 -0300

    News Service created

--- src/main/java/com/alkemy/ong/domain/service/NewsService.java
+++ src/main/java/com/alkemy/ong/domain/service/NewsService.java
@@ -1,8 +1,7 @@
 package com.alkemy.ong.domain.service;
-import com.alkemy.ong.domain.model.News;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface NewsService {
-    News save(News news);
+
 }

06873ce2d6e6db3069ac07a80d442a3770f22dc3
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-18 06:00:43 -0300

    NewsService method findById created

--- src/main/java/com/alkemy/ong/domain/service/NewsService.java
+++ src/main/java/com/alkemy/ong/domain/service/NewsService.java
@@ -1,7 +1,10 @@
 package com.alkemy.ong.domain.service;
+import com.alkemy.ong.domain.model.News;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface NewsService {
 
+    void delete(Long id);
+    News findById(Long id);
 }
--- src/main/java/com/alkemy/ong/domain/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/service/impl/NewsServiceImpl.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class NewsServiceImpl implements NewsService {
 
-
 }

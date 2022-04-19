3fed7089ad8741cab187365847448818d668687d
crisf5 <85839154+crisf5@users.noreply.github.com>
2022-04-19 11:54:12 -0300

    Endpoint listado de categorias (#7)
    Adding category findall, findById and save

---
+++ src/main/java/com/alkemy/ong/domain/categories/CategoryService.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.domain.categories;
+
+import com.alkemy.ong.domain.categories.CategoryGateway;
+import com.alkemy.ong.domain.categories.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryGateway categoryGateway;
+
+    public CategoryService(CategoryGateway categoryGateway){
+        this.categoryGateway = categoryGateway;
+    }
+
+    public List<Category> findAll(){
+        return categoryGateway.findAll();
+    }
+
+    public Category findById(Long id){return categoryGateway.findById(id);}
+
+    public Category save(Category category){return categoryGateway.save(category);}
+}

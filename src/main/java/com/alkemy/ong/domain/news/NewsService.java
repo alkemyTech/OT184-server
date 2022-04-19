dcabf63fec55fba8533f87000f736c117f6ed31b
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-19 13:07:56 -0300

    @Autowired annotations deleted

--- src/main/java/com/alkemy/ong/domain/news/NewsService.java
+++ src/main/java/com/alkemy/ong/domain/news/NewsService.java
@@ -1,5 +1,4 @@
 package com.alkemy.ong.domain.news;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/com/alkemy/ong/web/controllers/NewsController.java
+++ src/main/java/com/alkemy/ong/web/controllers/NewsController.java
@@ -3,7 +3,6 @@ import com.alkemy.ong.domain.news.News;
 import com.alkemy.ong.domain.news.NewsService;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;

383bb1d780881e296d1c2c5fc35ebab3c7e85053
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-19 03:53:34 -0300

    NewsService refactor, interface deleted

--- src/main/java/com/alkemy/ong/domain/services/NewsService.java
+++ src/main/java/com/alkemy/ong/domain/services/NewsService.java
@@ -1,9 +1,16 @@
 package com.alkemy.ong.domain.services;
+import com.alkemy.ong.domain.gateways.NewsGateway;
 import com.alkemy.ong.domain.models.News;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface NewsService {
+public class NewsService  {
 
-    News getDetails(Long id);
+    @Autowired
+    NewsGateway newsGateway;
+
+    public News getDetails(Long id) {
+        return newsGateway.getDetails(id);
+    }
 }

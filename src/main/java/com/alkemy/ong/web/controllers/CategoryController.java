11695669290a00d6937af7da70924b5b70f0e80f
Cristian <cristianfcba@gmail.com>
2022-04-20 17:04:34 -0300

    fix update in CategoryController

--- src/main/java/com/alkemy/ong/web/controllers/CategoryController.java
+++ src/main/java/com/alkemy/ong/web/controllers/CategoryController.java
@@ -48,13 +48,12 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
-//    @PutMapping("/{id}")
-//    private ResponseEntity<CategoryDTO> update(@PathVariable Long id, @Valid @RequestBody CategoryDTO categoryDTO){
-//        Category category = categoryService.findById(id);
-//        Category categoryUpdated = categoryService.update(category);
-//        return  ResponseEntity.ok().body(toDTO(categoryUpdated));
-//
-//    }
+    @PutMapping("/{id}")
+    private ResponseEntity<CategoryDTO> update(@PathVariable Long id, @Valid @RequestBody CategoryDTO categoryDTO){
+        Category categoryUpdated = categoryService.update(id, toModel(categoryDTO));
+        return  ResponseEntity.ok().body(toDTO(categoryUpdated));
+
+    }
 
     @Data
     @Builder

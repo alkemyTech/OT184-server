1f71f0346360405477e76ca46c86ec3274eadd8b
Miguel Berrio <53589014+mabmab159@users.noreply.github.com>
2022-04-20 13:16:11 -0500

    Actualizar y eliminar testimonios (#20)
    * Endpoint creacion de testimonios
    
    * Correccion endpoint testimonio
    
    * Actualizar y eliminar testimonios
    
    * Correccion en ruta
    
    * Correccion de endpoint
    
    * Correccion de rutas y actualizacion desde el main
    
    * Levantando errores, luego del merge
    
    * Agregando entorno de bd
    
    * Agregando variables de entorno
    
    * Correccion de Merge y cambiando respuestas

--- src/main/java/com/alkemy/ong/data/entities/ActivityEntity.java
+++ src/main/java/com/alkemy/ong/data/entities/ActivityEntity.java
@@ -18,27 +18,27 @@ import java.time.LocalDateTime;
 @SQLDelete(sql = "UPDATE activity SET deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
 public class ActivityEntity {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String name;
+    @Column(nullable = false)
+    private String name;
 
-  @Column(nullable = false)
-  private String content;
+    @Column(nullable = false)
+    private String content;
 
-  @Column(nullable = false)
-  private String image;
+    @Column(nullable = false)
+    private String image;
 
-  @Column(name = "created_at", updatable = false, nullable = false)
-  @CreationTimestamp
-  private LocalDateTime createdAt;
+    @Column(name = "created_at", updatable = false, nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-  @Column(name = "updated_at", nullable = false)
-  @UpdateTimestamp
-  private LocalDateTime updatedAt;
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-  @Column(name = "is_deleted", nullable = false)
-  private boolean isDeleted = Boolean.FALSE;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = Boolean.FALSE;
 }

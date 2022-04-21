a62e324cefd3e83c6f8f113158942b42ae4f98be
Cristian <cristianfcba@gmail.com>
2022-04-21 17:29:21 -0300

    defaultgateway created

---
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultSlideGateway.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.data.gateways;
+
+import com.alkemy.ong.domain.slide.Slide;
+import com.alkemy.ong.domain.slide.SlideGateway;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultSlideGateway implements SlideGateway {
+
+
+    @Override
+    public Slide findById(Long id) {
+        return null;
+    }
+}

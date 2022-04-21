d180d003e89d614469ea316dc113e2f73b0aa2ea
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-20 23:05:41 -0300

    add gateway update method

--- src/main/java/com/alkemy/ong/data/gateways/ActivityDefaultGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/ActivityDefaultGateway.java
@@ -4,7 +4,10 @@ import com.alkemy.ong.data.entities.ActivityEntity;
 import com.alkemy.ong.data.repositories.ActivityRepository;
 import com.alkemy.ong.domain.activities.Activity;
 import com.alkemy.ong.domain.activities.ActivityGateway;
+import com.alkemy.ong.web.controllers.ParamNotFound;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ActivityDefaultGateway implements ActivityGateway {
@@ -19,6 +22,23 @@ public class ActivityDefaultGateway implements ActivityGateway {
     ActivityEntity savedActivityEntity = activityRepository.save(toEntity(activity));
 
     return toModel(savedActivityEntity);
+  }
+
+  @Override
+  public Activity update(Long id, Activity activity) {
+    Optional<ActivityEntity> foundActivity = activityRepository.findById(id);
+    if (foundActivity.isEmpty()) {
+      throw new ParamNotFound("Activity id not valid");
+    }
+    refreshValues(foundActivity.get(), activity);
+    ActivityEntity savedActivity = activityRepository.save(foundActivity.get());
+    return toModel(savedActivity);
+  }
+
+  private void refreshValues(ActivityEntity activityEntity, Activity activity) {
+    activityEntity.setName(activity.getName());
+    activityEntity.setContent(activity.getContent());
+    activityEntity.setImage(activity.getImage());
   }
 
   private Activity toModel(ActivityEntity savedActivityEntity) {

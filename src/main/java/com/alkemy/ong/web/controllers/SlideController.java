707893ba5dc6c71a6d5583a150c12b799f42c6a9
Cristian <cristianfcba@gmail.com>
2022-04-21 17:19:24 -0300

    SlideController created

---
+++ src/main/java/com/alkemy/ong/web/controllers/SlideController.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/slide")
+public class SlideController {
+
+}

d7885fcb11109cd62cbe1c94cd05373588ffd2d1
Cristian <cristianfcba@gmail.com>
2022-04-21 17:25:45 -0300

    domain created

---
+++ src/main/java/com/alkemy/ong/domain/slide/Slide.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.domain.slide;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Slide {
+}
---
+++ src/main/java/com/alkemy/ong/domain/slide/SlideService.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.domain.slide;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SlideService {
+
+    private final SlideGateway slideGateway;
+
+    public SlideService(SlideGateway slideGateway){
+        this.slideGateway = slideGateway;
+    }
+
+    public Slide findById(Long id){return slideGateway.findById(id);}
+
+}

a9f5b20b354524ae6d62b113e12373ed5b79c06b
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-21 03:26:47 -0300

    method findall created

--- src/main/java/com/alkemy/ong/data/repositories/NewsRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/NewsRepository.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.data.repositories;
 
 import com.alkemy.ong.data.entities.NewsEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
-
+    Page<NewsEntity> findAll(Pageable pageRequest);
 }

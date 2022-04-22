c2a57e8b3197c6e4008fd767caa249e3548c416f
Cristian <cristianfcba@gmail.com>
2022-04-21 21:00:58 -0300

    DefaultSlideGateway findById completed

--- src/main/java/com/alkemy/ong/data/gateways/DefaultSlideGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultSlideGateway.java
@@ -1,5 +1,8 @@
 package com.alkemy.ong.data.gateways;
 
+import com.alkemy.ong.data.entities.SlidesEntity;
+import com.alkemy.ong.data.repositories.SlidesRepository;
+import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import com.alkemy.ong.domain.slide.Slide;
 import com.alkemy.ong.domain.slide.SlideGateway;
 import org.springframework.stereotype.Component;
@@ -7,9 +10,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class DefaultSlideGateway implements SlideGateway {
 
+    private final SlidesRepository slidesRepository;
+
+    public DefaultSlideGateway(SlidesRepository slidesRepository){
+        this.slidesRepository = slidesRepository;
+    }
 
     @Override
     public Slide findById(Long id) {
-        return null;
+        SlidesEntity slidesEntity = slidesRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("ID"));
+        return toModel(slidesEntity);
     }
+
+    public Slide toModel(SlidesEntity slidesEntity){
+        return Slide.builder()
+                .id(slidesEntity.getId())
+                .imageUrl(slidesEntity.getImageUrl())
+                .text(slidesEntity.getText())
+                .order(slidesEntity.getOrder())
+                .organization(slidesEntity.getOrganization())
+                .build();
+    }
+
+
 }

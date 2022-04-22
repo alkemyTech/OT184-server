c0ea525902b2a4882145f4c403d888af0222d3e9
Cristian <cristianfcba@gmail.com>
2022-04-21 21:08:21 -0300

    fix name Requestmapping in SlideController

--- src/main/java/com/alkemy/ong/web/controllers/SlideController.java
+++ src/main/java/com/alkemy/ong/web/controllers/SlideController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/slide")
+@RequestMapping("/slides")
 public class SlideController {
 
     private final SlideService slideService;

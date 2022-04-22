30de1917ffbfe14a1631721bfccb3d0efebf7530
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-21 22:40:53 -0300

    fixing unused imports

--- src/main/java/com/alkemy/ong/data/gateways/DefaultNewsGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultNewsGateway.java
@@ -11,8 +11,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 import static java.util.stream.Collectors.toList;
 
 @Component

00d6f927c73cf9b99af0c193c7d52d0f0995c03d
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-04-22 21:26:38 -0300

    add comment gateway

--- src/main/java/com/alkemy/ong/data/gateways/CommentDefaultGateway.java
+++ src/main/java/com/alkemy/ong/data/gateways/CommentDefaultGateway.java
@@ -1,7 +1,14 @@
 package com.alkemy.ong.data.gateways;
 
+import com.alkemy.ong.data.entities.CommentEntity;
+import com.alkemy.ong.data.entities.NewsEntity;
+import com.alkemy.ong.data.entities.UserEntity;
+import com.alkemy.ong.data.repositories.CommentRepository;
+import com.alkemy.ong.data.repositories.NewsRepository;
+import com.alkemy.ong.data.repositories.UserRepository;
 import com.alkemy.ong.domain.comments.Comment;
 import com.alkemy.ong.domain.comments.CommentGateway;
+import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +17,27 @@ import org.springframework.stereotype.Component;
 public class CommentDefaultGateway implements CommentGateway {
   private final CommentRepository commentRepository;
 
-  @Override
-  public Comment addComent(Long userId, String body, Long newsId) {
-    return commentRepository.save();
+  public Comment save(Comment comment) {
+    return toModel(commentRepository.save(toEntity(comment)));
+  }
+
+  private CommentEntity toEntity(Comment comment) {
+    return CommentEntity
+        .builder()
+        .id(comment.getId())
+        .userId(comment.getUserId())
+        .newsId(comment.getNewsId())
+        .body(comment.getBody())
+        .build();
+  }
+
+  private Comment toModel(CommentEntity commentEntity) {
+    return Comment
+        .builder()
+        .id(commentEntity.getId())
+        .userId(commentEntity.getUserId())
+        .newsId(commentEntity.getNewsId())
+        .body(commentEntity.getBody())
+        .build();
   }
 }

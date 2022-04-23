8e15c3f4e53563bca5b4a7cb4cf233c72fe765d7
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-23 16:42:23 -0300

    Controller constructor created

--- src/main/java/com/alkemy/ong/web/controllers/ContactController.java
+++ src/main/java/com/alkemy/ong/web/controllers/ContactController.java
@@ -1,7 +1,13 @@
 package com.alkemy.ong.web.controllers;
 
+import com.alkemy.ong.domain.contacts.ContactService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactController {
+    private final ContactService contactService;
+    public ContactController(ContactService contactService){
+        this.contactService = contactService;
+    }
+
 }

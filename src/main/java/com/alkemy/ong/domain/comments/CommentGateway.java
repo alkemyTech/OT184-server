a105e732b8b9776b6c577474d09cc72a7a364d8d
nicolaspucci1989 <nicolaspucci1989@gmail.com>
2022-04-24 20:18:59 -0300

    OT184-74 Create commentary model repository and service (#38)
    * add comment service
    
    * add comment gateway interface
    
    * add comment model
    
    * add partial comment default gateway
    
    * add comments service
    
    * add comment gateway interface
    
    * add comment entity
    
    * add comment repository
    
    * add comment gateway
    
    * add comment entity
    
    * add fetch type lazy
    
    * refactor: rename method to save
    
    * fix: add update and insert false
    
    * refactor: rename CommentDefaultGateway to DefaultCommentGateway
    
    * fix: sql delete typo
    
    * fix: sql delete field

---
+++ src/main/java/com/alkemy/ong/domain/comments/CommentGateway.java
@@ -0,0 +1,5 @@
+package com.alkemy.ong.domain.comments;
+
+public interface CommentGateway {
+  Comment save(Comment comment);
+}
---
+++ src/main/java/com/alkemy/ong/domain/comments/CommentService.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.domain.comments;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+  private final CommentGateway commentGateway;
+  public Comment save(Comment comment) {
+    return commentGateway.save(comment);
+  }
+}

c4fd4ddb82d3c01d3f98b2413176113fab40c476
Lucas Saleme <LucasEmiliano21@hotmail.com>
2022-04-23 21:16:56 -0300

    RequestMapping annotation added

--- src/main/java/com/alkemy/ong/web/controllers/ContactController.java
+++ src/main/java/com/alkemy/ong/web/controllers/ContactController.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @RestController
+@RequestMapping("contacts")
 public class ContactController {
     private final ContactService contactService;
     public ContactController(ContactService contactService){

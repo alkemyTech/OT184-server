6f41150b93bd4e84e898510c6850c873070a01f7
Cristian <cristianfcba@gmail.com>
2022-04-26 19:52:59 -0300

    EmailService completed

--- src/main/java/com/alkemy/ong/domain/email/EmailService.java
+++ src/main/java/com/alkemy/ong/domain/email/EmailService.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-//    private final EmailGateway emailGateway;
-//
-//    public EmailService(EmailGateway emailGateway){
-//        this.emailGateway = emailGateway;
-//    }
-//
-//    public sendMail(String to,String subject, String body){return emailGateway.sendmail(to,subject, body);}
+    private final EmailGateway emailGateway;
+
+    public EmailService(EmailGateway emailGateway){
+        this.emailGateway = emailGateway;
+    }
+
+    public String sendMail(String to, String subject, String body){return emailGateway.sendmail(to,subject, body);}
 }

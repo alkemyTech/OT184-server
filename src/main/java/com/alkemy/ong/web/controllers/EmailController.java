a9e738738a91267f44e8f9014bb044722a5ade28
Cristian <cristianfcba@gmail.com>
2022-04-26 19:54:14 -0300

    EmailController completed

--- src/main/java/com/alkemy/ong/web/controllers/EmailController.java
+++ src/main/java/com/alkemy/ong/web/controllers/EmailController.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.web.controllers;
 
+import com.alkemy.ong.domain.email.EmailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
 
-//    public ResponseEntity<String> sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body){
-//        String email = service(to, subject, body);
-//        return ResponseEntity.ok("Email sent successfully");
-//
-//    }
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService){
+        this.emailService = emailService;
+    }
+    public ResponseEntity<String> sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body){
+        String email = emailService.sendMail(to, subject, body);
+        return ResponseEntity.ok("Email sent successfully");
+    }
 }

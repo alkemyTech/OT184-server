677b0a781b098f3f460f479f4da232b748f65dd4
Cristian <cristianfcba@gmail.com>
2022-04-26 18:50:42 -0300

    DefaultEmailGateway created

---
+++ src/main/java/com/alkemy/ong/data/gateways/DefaultEmailGateway.java
@@ -0,0 +1,11 @@
+package com.alkemy.ong.data.gateways;
+
+import com.alkemy.ong.domain.email.EmailGateway;
+
+public class DefaultEmailGateway implements EmailGateway {
+
+
+    public String sendmail(String to, String subject, String body) {
+        return null;
+    }
+}

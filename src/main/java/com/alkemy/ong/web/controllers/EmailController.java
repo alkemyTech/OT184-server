85292264c5ceb8399ed2c612f87777a4361a47be
Cristian <cristianfcba@gmail.com>
2022-04-26 18:39:08 -0300

    add controller for send email

---
+++ src/main/java/com/alkemy/ong/web/controllers/EmailController.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+//    public ResponseEntity<String> sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String body){
+//        String email = service(to, subject, body);
+//        return ResponseEntity.ok("Email sent successfully");
+//
+//    }
+}

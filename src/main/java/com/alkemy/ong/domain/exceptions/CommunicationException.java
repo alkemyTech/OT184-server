4116e018547c7608a901c8fa4c495fa2d45b6a19
Cristian <cristianfcba@gmail.com>
2022-04-27 12:25:56 -0300

    fix exception string

--- src/main/java/com/alkemy/ong/domain/exceptions/CommunicationException.java
+++ src/main/java/com/alkemy/ong/domain/exceptions/CommunicationException.java
@@ -2,6 +2,6 @@ package com.alkemy.ong.domain.exceptions;
 
 public class CommunicationException extends RuntimeException{
     public CommunicationException(String param) {
-        super ("Error tryin to" + param);
+        super ("Error trying to" + param);
 }
 }

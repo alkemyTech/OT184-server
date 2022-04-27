c2298b79ece444d76b6a26574b597a97f8c8a2db
Cristian <cristianfcba@gmail.com>
2022-04-27 16:44:43 -0300

    add Native query for order slide

--- src/main/java/com/alkemy/ong/data/repositories/SlidesRepository.java
+++ src/main/java/com/alkemy/ong/data/repositories/SlidesRepository.java
@@ -3,8 +3,15 @@ package com.alkemy.ong.data.repositories;
 
 import com.alkemy.ong.data.entities.SlidesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SlidesRepository extends JpaRepository<SlidesEntity,Long> {
+
+    @Query(value = "SELECT * FROM slides s WHERE s.organization_id = :id ORDER BY s.slide_order",
+            nativeQuery = true)
+    List<SlidesEntity> orderBySlide(Long id);
 }

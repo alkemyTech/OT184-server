c4e12eba76ce6cb024f5598e34746bbb2d448e66
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-01 17:56:55 -0300

    add findByEmail to UserGateway

--- src/main/java/com/alkemy/ong/domain/users/UserGateway.java
+++ src/main/java/com/alkemy/ong/domain/users/UserGateway.java
@@ -1,14 +1,13 @@
 package com.alkemy.ong.domain.users;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 import java.util.List;
 
 public interface UserGateway {
-  List<Users> findAll();
+  List<User> findAll();
 
   void delete(Long id);
 
   UserDetails loadUserByUsername (String username) throws UsernameNotFoundException;
+
+  Users findByEmail(String email);
 }

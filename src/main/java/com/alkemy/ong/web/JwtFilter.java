d7bdd578481ae79ee36b36d2628f68248b4511df
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-01 00:37:27 -0300

    autowire attributes

--- src/main/java/com/alkemy/ong/web/JwtFilter.java
+++ src/main/java/com/alkemy/ong/web/JwtFilter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,15 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtRequestFilter extends OncePerRequestFilter {
-  private final UserService userService;
-  private final JwtUtil jwtUtil;
-
+@Component
+public class JwtFilter extends OncePerRequestFilter {
   @Autowired
-  public JwtRequestFilter(UserService userDetailsCustomService, JwtUtil jwtUtil) {
-    this.userService = userDetailsCustomService;
-    this.jwtUtil = jwtUtil;
-  }
+  private UserService userService;
+  @Autowired
+  private JwtUtil jwtUtil;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {

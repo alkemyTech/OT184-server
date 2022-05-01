66393c944dc28a50ed8b0ccc9e0e52498d87ef8e
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-01 00:40:06 -0300

    add auth controller

--- src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.web.controllers;
 
+<<<<<<< HEAD
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -8,36 +9,88 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+=======
+import com.alkemy.ong.domain.exceptions.CommunicationException;
+import com.alkemy.ong.domain.users.UserService;
+import com.alkemy.ong.domain.users.Users;
+import com.alkemy.ong.web.util.JwtUtil;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+>>>>>>> fd1bff5 (add auth controller)
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    private final AuthenticationManager authenticationManager;
+  private final AuthenticationManager authenticationManager;
+  private final PasswordEncoder passwordEncoder;
+  private final UserService userService;
+  private final JwtUtil jwtUtil;
 
-    public AuthenticationController(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+  public AuthenticationController(AuthenticationManager authenticationManager,
+                                  PasswordEncoder passwordEncoder,
+                                  UserService userService,
+                                  JwtUtil jwtUtil) {
+    this.authenticationManager = authenticationManager;
+    this.passwordEncoder = passwordEncoder;
+    this.userService = userService;
+    this.jwtUtil = jwtUtil;
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity.BodyBuilder signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
+    userService.save(
+        Users.builder()
+            .email(signUpRequestDto.getEmail())
+            .password(passwordEncoder.encode(signUpRequestDto.getPassword()))
+            .firstName(signUpRequestDto.getFirstName())
+            .lastName(signUpRequestDto.getLastName())
+            .build()
+    );
+    return ResponseEntity.ok();
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> singIn(@RequestParam("username") String username,
+                                  @RequestParam("password") String password) throws Exception {
+    UserDetails userDetails;
+    try {
+      Authentication auth = authenticationManager
+          .authenticate(new UsernamePasswordAuthenticationToken(username, password));
+      SecurityContextHolder.getContext().setAuthentication(auth);
+      userDetails = (UserDetails) auth.getPrincipal();
+    } catch (BadCredentialsException e) {
+      throw new CommunicationException("Incorrect username or password");
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> singIn(@RequestParam("username") String username, @RequestParam("password") String password) throws Exception {
-        Authentication auth = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        SecurityContextHolder.getContext().setAuthentication(auth);
+    String jwt = jwtUtil.generateToken(userDetails);
+    return ResponseEntity.ok(new AuthenticationResponse(jwt));
+  }
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(toAuthResponse(auth.getName()));
-    }
+  private AuthenticationResponse toAuthResponse(String auth) {
+    return AuthenticationResponse.builder().username(auth).build();
+  }
 
-   private AuthenticationResponse toAuthResponse(String auth) {
-        return AuthenticationResponse.builder().username(auth).build();
-    }
+  @Data
+  private static class SignUpRequestDto {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+  }
 
-    @Data
-    @Builder
-    private static class AuthenticationResponse {
-        private String username;
-    }
+  @Data
+  @Builder
+  private static class AuthenticationResponse {
+    private String username;
+  }
 }

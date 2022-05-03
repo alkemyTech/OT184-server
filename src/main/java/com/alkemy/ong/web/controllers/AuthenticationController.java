4076316a4626bed70e25d868f29a160fe34100e7
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-02 21:55:56 -0300

    fix auth controller

--- src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
@@ -11,9 +11,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 =======
 import com.alkemy.ong.domain.exceptions.CommunicationException;
+import com.alkemy.ong.domain.users.User;
 import com.alkemy.ong.domain.users.UserService;
-import com.alkemy.ong.domain.users.Users;
-import com.alkemy.ong.web.util.JwtUtil;
+import com.alkemy.ong.web.config.SecurityConfiguration.JwtUtil;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
@@ -31,66 +31,66 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-  private final AuthenticationManager authenticationManager;
-  private final PasswordEncoder passwordEncoder;
-  private final UserService userService;
-  private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-  public AuthenticationController(AuthenticationManager authenticationManager,
-                                  PasswordEncoder passwordEncoder,
-                                  UserService userService,
-                                  JwtUtil jwtUtil) {
-    this.authenticationManager = authenticationManager;
-    this.passwordEncoder = passwordEncoder;
-    this.userService = userService;
-    this.jwtUtil = jwtUtil;
-  }
-
-  @PostMapping("/register")
-  public ResponseEntity.BodyBuilder signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-    userService.save(
-        Users.builder()
-            .email(signUpRequestDto.getEmail())
-            .password(passwordEncoder.encode(signUpRequestDto.getPassword()))
-            .firstName(signUpRequestDto.getFirstName())
-            .lastName(signUpRequestDto.getLastName())
-            .build()
-    );
-    return ResponseEntity.ok();
-  }
-
-  @PostMapping("/login")
-  public ResponseEntity<?> singIn(@RequestParam("username") String username,
-                                  @RequestParam("password") String password) throws Exception {
-    UserDetails userDetails;
-    try {
-      Authentication auth = authenticationManager
-          .authenticate(new UsernamePasswordAuthenticationToken(username, password));
-      SecurityContextHolder.getContext().setAuthentication(auth);
-      userDetails = (UserDetails) auth.getPrincipal();
-    } catch (BadCredentialsException e) {
-      throw new CommunicationException("Incorrect username or password");
+    public AuthenticationController(AuthenticationManager authenticationManager,
+                                    PasswordEncoder passwordEncoder,
+                                    UserService userService,
+                                    JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
-    String jwt = jwtUtil.generateToken(userDetails);
-    return ResponseEntity.ok(new AuthenticationResponse(jwt));
-  }
+    @PostMapping("/register")
+    public ResponseEntity.BodyBuilder signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
+        userService.save(
+                User.builder()
+                        .email(signUpRequestDto.getEmail())
+                        .password(passwordEncoder.encode(signUpRequestDto.getPassword()))
+                        .firstName(signUpRequestDto.getFirstName())
+                        .lastName(signUpRequestDto.getLastName())
+                        .build()
+        );
+        return ResponseEntity.ok();
+    }
 
-  private AuthenticationResponse toAuthResponse(String auth) {
-    return AuthenticationResponse.builder().jwt(auth).build();
-  }
+    @PostMapping("/login")
+    public ResponseEntity<?> singIn(@RequestParam("username") String username,
+                                    @RequestParam("password") String password) throws Exception {
+        UserDetails userDetails;
+        try {
+            Authentication auth = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            userDetails = (UserDetails) auth.getPrincipal();
+        } catch (BadCredentialsException e) {
+            throw new CommunicationException("Incorrect username or password");
+        }
 
-  @Data
-  private static class SignUpRequestDto {
-    private String email;
-    private String password;
-    private String firstName;
-    private String lastName;
-  }
+        String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
 
-  @Data
-  @Builder
-  private static class AuthenticationResponse {
-    private String jwt;
-  }
+    private AuthenticationResponse toAuthResponse(String auth) {
+        return AuthenticationResponse.builder().jwt(auth).build();
+    }
+
+    @Data
+    private static class SignUpRequestDto {
+        private String email;
+        private String password;
+        private String firstName;
+        private String lastName;
+    }
+
+    @Data
+    @Builder
+    private static class AuthenticationResponse {
+        private String jwt;
+    }
 }

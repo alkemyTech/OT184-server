7b984ee83bb97d3a1bc32270e31b12cdfde656ea
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-05-02 22:08:47 -0300

    rollback(register): remove simple register endpoint

--- src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/web/controllers/AuthenticationController.java
@@ -1,18 +1,6 @@
 package com.alkemy.ong.web.controllers;
 
-<<<<<<< HEAD
-import lombok.Builder;
-import lombok.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-=======
 import com.alkemy.ong.domain.exceptions.CommunicationException;
-import com.alkemy.ong.domain.users.User;
-import com.alkemy.ong.domain.users.UserService;
 import com.alkemy.ong.web.config.SecurityConfiguration.JwtUtil;
 import lombok.Builder;
 import lombok.Data;
@@ -23,45 +11,26 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
->>>>>>> fd1bff5 (add auth controller)
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
-    private final UserService userService;
     private final JwtUtil jwtUtil;
 
-    public AuthenticationController(AuthenticationManager authenticationManager,
-                                    PasswordEncoder passwordEncoder,
-                                    UserService userService,
-                                    JwtUtil jwtUtil) {
+    public AuthenticationController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
-        this.passwordEncoder = passwordEncoder;
-        this.userService = userService;
         this.jwtUtil = jwtUtil;
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity.BodyBuilder signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-        userService.save(
-                User.builder()
-                        .email(signUpRequestDto.getEmail())
-                        .password(passwordEncoder.encode(signUpRequestDto.getPassword()))
-                        .firstName(signUpRequestDto.getFirstName())
-                        .lastName(signUpRequestDto.getLastName())
-                        .build()
-        );
-        return ResponseEntity.ok();
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> singIn(@RequestParam("username") String username,
-                                    @RequestParam("password") String password) throws Exception {
+                                    @RequestParam("password") String password) {
         UserDetails userDetails;
         try {
             Authentication auth = authenticationManager
@@ -73,19 +42,11 @@ public class AuthenticationController {
         }
 
         String jwt = jwtUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        return ResponseEntity.ok(toAuthResponse(jwt));
     }
 
     private AuthenticationResponse toAuthResponse(String auth) {
         return AuthenticationResponse.builder().jwt(auth).build();
-    }
-
-    @Data
-    private static class SignUpRequestDto {
-        private String email;
-        private String password;
-        private String firstName;
-        private String lastName;
     }
 
     @Data

72c587d968989f70f0f7672f218f2d83e9be2120
MabMab^^ <miguelberrioh@hotmail.com>
2022-05-04 08:36:41 -0500

    Agregando mensaje de bienvenida

---
+++ src/main/java/com/alkemy/ong/domain/mail/MailService.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.domain.mail;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private final MailGateway mailGateway;
+
+    public MailService(MailGateway mailGateway) {
+        this.mailGateway = mailGateway;
+    }
+
+    public Boolean sendMail(String to, String subject) {
+        return mailGateway.sendMail(to, subject);
+    }
+
+}
--- src/main/java/com/alkemy/ong/web/controllers/UserController.java
+++ src/main/java/com/alkemy/ong/web/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.web.controllers;
 
+import com.alkemy.ong.domain.mail.MailGateway;
 import com.alkemy.ong.domain.roles.Role;
 import com.alkemy.ong.domain.users.UserService;
 import com.alkemy.ong.domain.users.Users;
@@ -14,67 +15,70 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/users")
 public class UserController {
-  private final UserService userService;
+    private final UserService userService;
+    private final MailGateway mailGateway;
 
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
+    public UserController(UserService userService, MailGateway mailGateway) {
+        this.userService = userService;
+        this.mailGateway = mailGateway;
+    }
 
-  @GetMapping
-  public ResponseEntity<List<UserDto>> findAll() {
-    return ResponseEntity.ok().body(toListDto(userService.findAll()));
-  }
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok().body(toListDto(userService.findAll()));
+    }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id){
-    userService.delete(id);
-    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-  }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
-  private List<UserDto> toListDto(List<Users> users) {
-    return users.stream()
-        .map(this::toDto)
-        .collect(toList());
-  }
+    private List<UserDto> toListDto(List<Users> users) {
+        return users.stream()
+                .map(this::toDto)
+                .collect(toList());
+    }
 
-  private UserDto toDto(Users user) {
-    return UserDto.builder()
-        .id(user.getId())
-        .firstName(user.getFirstName())
-        .lastName(user.getLastName())
-        .email(user.getEmail())
-        .photo(user.getPhoto())
-        .role(roleToDto(user))
-        .build();
-  }
+    private UserDto toDto(Users user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .photo(user.getPhoto())
+                .role(roleToDto(user))
+                .build();
+    }
 
-  private RoleDto roleToDto(Users user) {
-    Role role = user.getRole();
-    return RoleDto.builder()
-        .id(role.getId())
-        .name(role.getName())
-        .description(role.getDescription())
-        .build();
-  }
+    private RoleDto roleToDto(Users user) {
+        Role role = user.getRole();
+        return RoleDto.builder()
+                .id(role.getId())
+                .name(role.getName())
+                .description(role.getDescription())
+                .build();
+    }
 
-  @Builder
-  @Data
-  public static class RoleDto {
-    private Long id;
-    private String name;
-    private String description;
-  }
+    @Builder
+    @Data
+    public static class RoleDto {
+        private Long id;
+        private String name;
+        private String description;
+    }
 
-  @Builder
-  @Data
-  public static class UserDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String photo;
-    private RoleDto role;
-  }
+    @Builder
+    @Data
+    public static class UserDto {
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String photo;
+        private RoleDto role;
+    }
 }

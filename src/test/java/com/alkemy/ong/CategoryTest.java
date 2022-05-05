2209c767c3eb4614ed511e77a3f4bf906cc8585a
Cristian <cristianfcba@gmail.com>
2022-05-05 10:48:16 -0300

    find by id test created

--- src/test/java/com/alkemy/ong/CategoryTest.java
+++ src/test/java/com/alkemy/ong/CategoryTest.java
@@ -2,6 +2,7 @@ package com.alkemy.ong;
 
 import com.alkemy.ong.data.entities.CategoryEntity;
 import com.alkemy.ong.data.repositories.CategoryRepository;
+import com.alkemy.ong.domain.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.mockito.ArgumentMatchers.isNotNull;
@@ -59,6 +61,31 @@ public class CategoryTest {
                 .andExpect((ResultMatcher) jsonPath("$[1].id", is(2)))
                 .andExpect((ResultMatcher) jsonPath("$[0].name", is("Health")))
                 .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void getCategoryByIDTest() throws Exception{
+        var categoryEntity = createCategory(1L,"Health", "Health is very important for the world", "health.jpg");
+
+        when(categoryRepository.findById(categoryEntity.getId())).thenReturn(Optional.of(categoryEntity));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/categories/1").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(categoryEntity)))
+                .andExpect((ResultMatcher) jsonPath("$.id", is(1)))
+                .andExpect((ResultMatcher) jsonPath("$.name", is("Health")))
+                .andExpect((ResultMatcher) jsonPath("$.description", is("Health is very important for the world")))
+                .andExpect((ResultMatcher) jsonPath("$.image", is("health.jpg")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getCategoryByIDTestError() throws Exception{
+
+        when(categoryRepository.findById(1234L)).thenThrow(ResourceNotFoundException.class);
+
+        mockMvc.perform((MockMvcRequestBuilders.get("/categories/1234")).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
 
     }
 

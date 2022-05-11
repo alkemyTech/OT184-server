75ef14ad312864113e41fae4f2cfc52c6798c02e
ANDRES PUEBLA <92341430+pueblandres@users.noreply.github.com>
2022-05-11 14:09:38 -0300

    Send contact email (#76)
    * "first commit"
    
    * "post slide"
    
    * updating minor changes to aws image upload
    
    * send email
    
    Co-authored-by: lucas.paoliello <paoliellolucas@gmail.com>

--- src/main/java/com/alkemy/ong/domain/contacts/ContactService.java
+++ src/main/java/com/alkemy/ong/domain/contacts/ContactService.java
@@ -1,14 +1,21 @@
 package com.alkemy.ong.domain.contacts;
 
+import com.alkemy.ong.domain.email.EmailService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ContactService {
+
+    private final String texto = "Gracias por contactarte con nosotros ";
+
     private final ContactGateway contactGateway;
-    public ContactService(ContactGateway contactGateway){
+    private final EmailService emailService;
+
+    public ContactService(ContactGateway contactGateway, EmailService emailService){
         this.contactGateway = contactGateway;
+        this.emailService = emailService;
     }
 
     public List<Contact> findAll() {
@@ -16,6 +23,10 @@ public class ContactService {
     }
 
     public Contact save(Contact contact) {
+
+        emailService.sendMail(contact.getEmail(), "Bienvenido a Alkemy " + contact.getName(), texto , "Bienvenido " + contact.getName());
+
+
         return contactGateway.save(contact);
     }
 }
